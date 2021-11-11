599ddb5340dfdf551e3d544546c92cf6665c8dbe
Skyseras <yasserelmahfoudi@gmail.com>
2021-11-11 11:05:22 +0100

    Done

---
+++ Challenge3.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+
+    float Distance;
+
+    printf("Entrez la distance en metre : ");
+    scanf("%f", &Distance);
+
+    Distance = Distance/1000*1.609;
+
+    printf("La distance en Mile est : %f", Distance);
+
+    return 0;
+} 
